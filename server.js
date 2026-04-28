5646e73905eadd4941ade22ef225ae8c34a2e835
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-28 20:01:34 +0200

    starting with server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const http = require('http');
+const fs = require('fs');
 const secret = require('./src/secret');
+
+http.createServer((req,res)=>{
+    switch (req.url.split('?')[0]) {
+        case "/":
+            break;
+        case "/api/create":
+            break;
+        case "/api/verify":
+            break;
+    }
+}).listen(4824);
