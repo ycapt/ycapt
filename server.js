736013d9199cbb805999f58503a77d4eb1e0f62f
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-28 20:09:52 +0200

    made a website

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const secret = require('./src/secret');
 http.createServer((req,res)=>{
     switch (req.url.split('?')[0]) {
         case "/":
+            res.end(fs.readFileSync('./site.html'));
             break;
         case "/api/create":
             break;
